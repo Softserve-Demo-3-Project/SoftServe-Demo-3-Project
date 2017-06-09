5c2824951856a23e797a3b219823b720c8e77601
Valery Hristov <vhristov@softserveinc.com>
2017-06-09 20:51:00 +0300

    adding default route

--- src/demo/demoModule.js
+++ src/demo/demoModule.js
@@ -1,8 +1,18 @@
 import angular from 'angular';
+import router from 'angular-route';
 import welcomeDirective from './welcome/welcomeDirective';
 
 export default angular
-  .module('demo', [])
+  .module('demo', ['ngRoute'])
+  .config(function($routeProvider) {
+	$routeProvider
+	  .when('/', {
+	  	template: '<demo-welcome></demo-welcome>'
+	  })
+	  .otherwise({
+	  	template: "nqma takoa"
+	  })
+  })
   .directive('demoWelcome', welcomeDirective)
   .name;
 
