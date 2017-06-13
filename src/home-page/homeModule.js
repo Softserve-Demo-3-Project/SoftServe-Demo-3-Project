c340f41bf8d7dff9b765d805e16a77895fcd1041
Thenis <d.stoilov96@gmail.com>
2017-06-13 11:22:18 +0300

    Moved service in different folder /core/services/ads

--- src/home-page/homeModule.js
+++ src/home-page/homeModule.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import ngRoute from 'angular-route';
 import homeDirective from './homeDirective.js';
-import { AdsService } from './adsFactory.js';
+import { AdsService } from './../core/services/ads/adsService.js';
 
 export default angular
     .module('home', ['ngRoute'])
