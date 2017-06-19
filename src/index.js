c4b53c5c01df804bdc97a41004cb5cb0f5cbf3ef
Mihaelaetr <mihaelaetr>
2017-06-19 16:02:41 +0300

    Added authentication service layer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import router from 'angular-route';
+import ngCookies from 'angular-cookies';
 
 // Models
 import homeDirective from './home-page/homeDirective';
@@ -9,7 +10,8 @@ import publishDirective from './publish-page/publishDirective';
 
 // Common
 import { AdsService } from './core/services/adsService';
-import { UserService } from './core/services/userService';
+import { UserService } from './core/services/user.service';
+import { AuthenticationService } from './core/services/authentication.service';
 import navigationDirective from './core/directive/navigation/navigationDirective';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -18,7 +20,7 @@ import './index.scss';
 
 // import demoModule from './demo/demoModule';
 
-angular.module('main', ['ngRoute'])
+angular.module('main', ['ngRoute', 'ngCookies'])
   .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
     // Remove "!" from url
@@ -51,6 +53,27 @@ angular.module('main', ['ngRoute'])
   .directive('navigation', navigationDirective)
   .service('AdsService', AdsService)
   .service('userService', UserService)
+  .service('authenticationService', AuthenticationService)
+  .run(['$rootScope', '$location', '$cookies', '$http',
+    function($rootScope, $location, $cookies, $http) {
+      // keep user logged in after page refresh
+      $rootScope.globals = $cookies.getObject('globals') || {};
+      if ($rootScope.globals.currentUser) {
+          $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+      }
+
+      $rootScope.$on('$locationChangeStart', function (event, next, current) {
+          // redirect to login page if not logged in and trying to access a restricted page
+          let pages = ['/login', '/register'];
+          //let restrictedPage = pages.indexOf($location.path()) === -1;
+          //TODO add restriction conditions for guests
+          let restrictedPage = false; // no restrictions
+          let loggedIn = $rootScope.globals.currentUser;
+          if (restrictedPage && !loggedIn) {
+              $location.path('/login');
+          }
+      });
+    }])
   .name;
 
 angular.bootstrap(document.documentElement, ['main']);
--- src/login-page/LoginController.js
+++ src/login-page/LoginController.js
@@ -1,33 +1,38 @@
 
 export default class LoginController {
-    constructor(userService){
-    	this.userService=userService;
+    constructor(userService, authenticationService, $location){
+    	this.userService = userService;
+    	this.authenticationService = authenticationService;
+    	this.$location = $location;
         this.init();
     }
-}
 
-LoginController.prototype.init = function () {
-	this.username = "";
-	this.password = "";
-	this.rememberMe = false;
-	this.hasFailed = false;
-	this.hasLoggedIn = false;
-}
+    init() {
+    	this.username = "";
+    	this.password = "";
+    	this.rememberMe = false;
+    	this.hasFailed = false;
+    	this.hasLoggedIn = false;
+    }
 
-LoginController.prototype.submit = function () {
-	let _this = this;
-	this.hasFailed = false;
-	this.hasLoggedIn = false;
+    submit() {
+    	let _this = this;
+    	let username = this.username;
+    	let password = this.password;
+    	this.hasFailed = false;
+    	this.hasLoggedIn = false;
 
-	this.userService.login(this.username, this.password).then(function(response) {
-		let data = response.data;
+    	this.userService.login(username, password).then(function(response) {
+    		let data = response.data;
 
-		if (data.length) {
-			let user = data[0];
-			_this.hasLoggedIn = true;
-			//TODO login success
-		} else {
-			_this.hasFailed = true;
-		}
-	});
+    		if (data.length) {
+    			let user = data[0];
+    			_this.hasLoggedIn = true;
+    			_this.authenticationService.setCredentials(username, password);
+    			_this.$location.path('/');
+    		} else {
+    			_this.hasFailed = true;
+    		}
+    	});
+    }
 }
