f73c64cf22a6a3742a76ebe3cc7932abe0e15a8c
Petar Kolev <petar_g_kolev@abv.bg>
2017-06-21 19:25:44 +0300

    Update user.service.js

--- src/core/services/user.service.js
+++ src/core/services/user.service.js
@@ -13,4 +13,20 @@ export  class UserService {
 		    }
         });
     }
+
+    register(name, age, email, username, password) {
+        return this.$http({
+            method: 'POST',
+            url: 'http://localhost:3000/users',
+            params: {
+                name: name,
+                age: age,
+                email: email,
+                username: username,
+                password: password
+            }
+        }).success(function(data) {
+            $window.location.href('/');
+        });
+    }
 }
