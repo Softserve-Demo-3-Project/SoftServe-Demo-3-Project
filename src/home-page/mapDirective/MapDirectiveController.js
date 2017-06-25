13bb73663b41ad1dd966312fb4628b41cccb31ad
ivailoAndonov <ivailoandonov.andonov@gmail.com>
2017-06-25 16:25:06 +0300

    fix map service

--- src/home-page/mapDirective/MapDirectiveController.js
+++ src/home-page/mapDirective/MapDirectiveController.js
@@ -1,18 +1,10 @@
 export default class MapDirectiveController {
     constructor($scope, mapService) {
 
-        // Get coordinate
-
-console.log($scope);
-
-// console.log($scope.ads);
-
-        // Load map
         mapService.loadScript()
         .then(() => {
             // console.log();
-            mapService.initialize();
-
+            mapService.initialize($scope.ads);
         });
     }
 }
