96a9eb71ef8f7116f462e8e9eedad273c6d7a111
Valeri Hristov <valericfbg@gmail.com>
2017-06-25 15:34:39 +0300

    fixing race condition of the component rendering before there is any data

--- src/core/services/map.service.js
+++ src/core/services/map.service.js
@@ -27,37 +27,40 @@ export class mapService {
             return deferred.promise;
         }
 
-        function initialize() {
+        function initialize(ads) {
 
 
-            var cities = [
-                {
-                    lat: 23.200000,
-                    long: 79.225487
-                },
-                {
-                    lat: 28.500000,
-                    long: 77.250000
-                },
-                {
-                    lat: 22.500000,
-                    long: 88.400000
-                },
-                {
-                    lat: 19.000000,
-                    long: 72.90000
-                },
-                {
-                    lat: 12.9667,
-                    long: 77.5667
-                }
-            ];
+            // var cities = [
+            //     {
+            //         lat: 23.200000,
+            //         long: 79.225487
+            //     },
+            //     {
+            //         lat: 28.500000,
+            //         long: 77.250000
+            //     },
+            //     {
+            //         lat: 22.500000,
+            //         long: 88.400000
+            //     },
+            //     {
+            //         lat: 19.000000,
+            //         long: 72.90000
+            //     },
+            //     {
+            //         lat: 12.9667,
+            //         long: 77.5667
+            //     }
+            // ];
+            var cities = ads
+              .filter(ad => ad.locationLat && ad.locationLng)
+              .map((ad) => ({lat: ad.locationLat, long: ad.locationLng}));
 
             // var coordinate = [{}];
 
             var mapOptions = {
                 zoom: 4,
-                center: new google.maps.LatLng(cities[0].lat, cities[0].long)
+                center: new google.maps.LatLng(cities[0] && cities[0].lat, cities[0] && cities[0].long)
             };
 
             var map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
--- src/home-page/mapDirective/MapDirectiveController.js
+++ src/home-page/mapDirective/MapDirectiveController.js
@@ -1,17 +1,9 @@
 export default class MapDirectiveController {
     constructor($scope, mapService) {
-
-        // Get coordinate
-
-console.log($scope);
-
-// console.log($scope.ads);
-
-        // Load map
         mapService.loadScript()
         .then(() => {
             // console.log();
-            mapService.initialize();
+            mapService.initialize($scope.ads);
 
         });
     }
